d8f1ca7f49156df610a83e3785e6e54b867dec75
unknown <ssk66221@gmail.com>
2025-06-28 10:17:58 +0545

    feat(feature): project setup for ai career coach agent

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
+import Provider from "./provider";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "AI Career Coach Agent",
+  description: "AI Career Coach Agent",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Provider>
+            {children}
+
+          </Provider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
